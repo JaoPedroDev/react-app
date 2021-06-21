3744b2650dd39bf3b26476310cc87ce4c4795f76
JaoPedroDev <joaopedropessoal.15@gmail.com>
2021-06-21 20:26:18 -0300

    Teste de commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Uau mano que top
         </p>
         <a
           className="App-link"
